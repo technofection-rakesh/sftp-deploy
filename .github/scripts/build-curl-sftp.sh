1e3cd58cac39dde5c000ca17cb9cbf8c10cd0512
rakesh-mehta-83 <rakesh@vibgyortechsolutions.com>
2022-12-13 19:41:19 +0530

    curl install script modified

--- .github/scripts/build-curl-sftp.sh
+++ .github/scripts/build-curl-sftp.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-export CPPFLAGS=-I/usr/local/include
-export LDFLAGS="-L/usr/local/lib -Wl,-rpath,/usr/local/lib"
-export LIBS="-ldl"
-
-sudo sed -i -e "s/# deb-src /deb-src /g" /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y build-essential debhelper libssh-dev
-
-cd /opt/
-
-# Fetch cURL source code
-sudo apt-get source curl
-sudo apt-get build-dep -y curl
-cd curl-*/debian/ && sudo sed -i -e "s@CONFIGURE_ARGS += --without-libssh2@CONFIGURE_ARGS += --with-libssh2@g" rules
-cd curl-*/ && sudo dpkg-buildpackage -uc -us -b
-
-# Install rebuilding packages
-sudo dpkg -i curl_*.deb
-sudo dpkg -i libcurl3-*.deb
-sudo dpkg -i libcurl3-gnutls_*.deb
-sudo dpkg -i libcurl4-gnutls-dev_7.58.0-2ubuntu3.8_amd64.deb libcurl4_7.58.0-2ubuntu3.8_amd64.deb
+cd /opt
+wget https://curl.se/download/curl-7.86.0.tar.gz
+tar -xvzf curl-7.86.0.tar.gz
+rm curl-7.86.0.tar.gz
+sudo apt install libssh2-1-dev libssl-dev
+cd curl-7.86.0/
+./configure --with-ssl --with-libssh2
+sudo make
+sudo make install
